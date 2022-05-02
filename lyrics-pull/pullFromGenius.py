1979ec5c8aeb2084195ceb04a206eb6ec77b956d
Brandon <brandonlbmx98@gmail.com>
2022-05-02 01:39:10 -0500

    added UL lyrics

--- lyrics-pull/pullFromGenius.py
+++ lyrics-pull/pullFromGenius.py
@@ -2,15 +2,15 @@
 import lyricsgenius as genius
 import json
 
-geniusCreds = "TOKEN HERE"
+geniusCreds = "TOKEN"
 artist_name = "Red Hot Chili Peppers"
 
 api = genius.Genius(geniusCreds)
-artist = api.search_artist(artist_name, max_songs=278)
+artist = api.search_artist(artist_name, max_songs=300)
 
 rhcp = []
 for song in artist.songs:
     rhcp.append({'title': song.title, 'lyrics': song.lyrics, 'art': song.song_art_image_thumbnail_url, 'url': song.url, 'state': song.lyrics_state})
 
-with open('test.json', 'w') as fout:
+with open('rhcplyrics.json', 'w') as fout:
     json.dump(rhcp , fout)
