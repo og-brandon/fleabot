9f084c6d69f1842786380dd89669647738b4fa5e
Alexander Erben <info@it-erben.com>
2022-05-10 21:30:13 +0200

    :xRevert "Merge pull request #5 from Funky-Monks/role-based-time"
    This reverts commit 987f2ac29715c8a23ad466f5639d5a01e41bed0b, reversing
    changes made to 9fc0e7a37b78895671672ca89165ab566eced799.

--- src/commands/time.ts
+++ src/commands/time.ts
@@ -1,22 +1,24 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import {
-  Collection,
   CommandInteraction,
   EmbedFieldData,
   MessageEmbed,
-  Role,
+  User,
 } from "discord.js";
 import moment from "moment-timezone";
+import { client } from "../utils";
+import {logger} from "../logger";
 import fs from "fs";
-import { Command } from "./command";
-import { Snowflake } from "discord-api-types/v9";
+import {Command} from "./command";
 
 const config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
 
-export const timeCommand: Command = {
+const knownUsersToTimezones: Record<string, string[]> = config.tzUserRegistry;
+
+export const timeCommand: Command =  {
   data: new SlashCommandBuilder()
     .setName("coffeetime")
-    .setDescription("Calculates the current time for server members")
+    .setDescription("Calculates the current time for Coffee Shop members")
     .addNumberOption((option) =>
       option
         .setName("offset-hours")
@@ -29,30 +31,23 @@ export const timeCommand: Command = {
     ) as SlashCommandBuilder,
   async execute(interaction: CommandInteraction) {
     await interaction.deferReply();
+
     const offset: number =
-        interaction.options.getNumber("offset-hours", false) || 0;
+      interaction.options.getNumber("offset-hours", false) || 0;
 
-    const roles: Collection<Snowflake, Role> =
-      (await interaction.guild?.roles?.fetch()) ||
-      new Collection<Snowflake, Role>();
-    const tzRoles = roles.filter((role) => role.name.startsWith("TZ"))
     const fields: EmbedFieldData[] = [];
-
-    for (let role of tzRoles.values()) {
-      const tz = role.name.replace("TZ:", "")
+    for (let tz of Object.keys(knownUsersToTimezones)) {
+      const userIds: string[] = knownUsersToTimezones[tz];
+      const users = await Promise.all(userIds.map(userId => client.users.fetch(userId)))
       const m = moment().utc().add(offset, "hours").tz(tz, false);
       fields.push({
         name: m.format("HH:mm:ss"),
-        value: role.members.map((member) => member.user.username).join(", "),
+        value: users.map((user) => user.username).join(", "),
       });
     }
 
     const passEmbed = new MessageEmbed()
-      .setTitle(
-        `Current local times of your fellow members ${
-          offset !== 0 ? `in ${offset} hours` : ""
-        }`
-      )
+      .setTitle(`Current local times of coffee fellows ${offset !== 0 ? `in ${offset} hours` : ""}`)
       .addFields(fields)
       .setColor("#c8ff00");
     await interaction.editReply({ embeds: [passEmbed] });
